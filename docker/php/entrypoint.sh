528fc710c6469fc149f5d914cadaf89780dab732
Daniel Casas <dcasas@gyleven.com>
2025-06-02 09:20:15 +0200

    first commit

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pernisos Laravel
+echo "🔧 Fixing Laravel storage/cache permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+exec apache2-foreground
+
